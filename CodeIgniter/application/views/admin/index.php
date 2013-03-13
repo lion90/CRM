75bad7dbe77a79c3b191e1f70b09d28ae75322da
Dennis Zepeda <dennis.zepeda@hotmail.com>
2013-03-13 00:54:22 -0600

    Cambios en el Menú
    Le quite las tablas a todos los menús

--- CodeIgniter/application/views/admin/index.php
+++ CodeIgniter/application/views/admin/index.php
@@ -2,28 +2,28 @@
 <ul id="accordion">
 	
 <li>
-		<a  href="#" class="item op1Over2" rel="op1"><table style="width:167px;"><tr><td><img src="<?php echo base_url(); ?>style/imagenes/grafica.png" border="0" style="width:19px;height:19px" ></img></td><td><b>Ver Graficas</b></td><tr></table></a>
+		<a  href="#" class="item op1Over2" rel="op1"><img src="<?php echo base_url(); ?>style/imagenes/grafica.png" border="0" style="width:19px;height:19px; float: left; padding-left: 10px;" ></img><b>Ver Graficas</b></a>
 		<ul >
-			<li><a class="s" href="#"><table style="width:167px;"><tr><td><img src="<?php echo base_url(); ?>style/imagenes/grafica.png" border="0" style="width:19px;height:19px" ></img></td><td>Encuesta</td></tr></table></a></li>	
-			<li><a class="s" href="#"><table style="width:167px;"><tr><td><img src="<?php echo base_url(); ?>style/imagenes/open.png" border="0" style="width:19px;height:19px"></td><td>Open House</td></tr></table></a></li>
-			<li><a class="s" href="#"><table style="width:167px;"><tr><td><img src="<?php echo base_url(); ?>style/imagenes/grafica.png" border="0" style="width:19px;height:19px" ></img></td><td>Paquete</td></tr></table></a></li>	
-			<li><a class="s" href="#"><table style="width:167px;"><tr><td><img src="<?php echo base_url(); ?>style/imagenes/grafica.png" border="0" style="width:19px;height:19px"></td><td>Incripcion</td></tr></table></a></li>
+			<li><a class="s" href="#"><img src="<?php echo base_url(); ?>style/imagenes/grafica.png" border="0" style="width:19px;height:19px; float: left; padding-left: 10px;" ></img>Encuesta</a></li>	
+			<li><a class="s" href="#"><img src="<?php echo base_url(); ?>style/imagenes/open.png" border="0" style="width:19px;height:19px; float: left; padding-left: 10px;">Open House</a></li>
+			<li><a class="s" href="#"><img src="<?php echo base_url(); ?>style/imagenes/grafica.png" border="0" style="width:19px;height:19px; float: left; padding-left: 10px;" ></img>Paquete</a></li>	
+			<li><a class="s" href="#"><img src="<?php echo base_url(); ?>style/imagenes/grafica.png" border="0" style="width:19px;height:19px; float: left; padding-left: 10px;">Incripcion</a></li>
 		</ul>
 	</li>
 	<li>
-		<a  href="#" class="item op1Over2" rel="op1"><table style="width:167px;"><tr><td><img src="<?php echo base_url(); ?>style/imagenes/informe.png" border="0" style="width:19px;height:19px" ></img></td><td><b>Ingresar Documento</b></td><tr></table></a>
+		<a  href="#" class="item op1Over2" rel="op1"><img src="<?php echo base_url(); ?>style/imagenes/informe.png" border="0" style="width:19px;height:19px; float: left; padding-left: 10px;" ></img><b>Ingresar Documento</b></a>
 		<ul >
-			<li><a id="ing_encuesta" class="s" href="#"><table style="width:167px;"><tr><td><img src="<?php echo base_url(); ?>style/imagenes/informe.png" border="0" style="width:19px;height:19px" ></img></td><td>Encuesta</td></tr></table></a></li>	
-			<li><a class="s" href="#"><table style="width:167px;"><tr><td><img src="<?php echo base_url(); ?>style/imagenes/informe.png" border="0" style="width:19px;height:19px"></td><td>Open House</td></tr></table></a></li>
-			<li><a class="s" href="#"><table style="width:167px;"><tr><td><img src="<?php echo base_url(); ?>style/imagenes/informe.png" border="0" style="width:19px;height:19px" ></img></td><td>Paquete</td></tr></table></a></li>	
-			<li><a class="s" href="#"><table style="width:167px;"><tr><td><img src="<?php echo base_url(); ?>style/imagenes/informe.png" border="0" style="width:19px;height:19px"></td><td>Incripcion</td></tr></table></a></li>
+			<li><a id="ing_encuesta" class="s" href="#"><img src="<?php echo base_url(); ?>style/imagenes/informe.png" border="0" style="width:19px;height:19px; float: left; padding-left: 10px;" ></img>Encuesta</a></li>	
+			<li><a class="s" href="#"><img src="<?php echo base_url(); ?>style/imagenes/informe.png" border="0" style="width:19px;height:19px; float: left; padding-left: 10px;">Open House</a></li>
+			<li><a class="s" href="#"><img src="<?php echo base_url(); ?>style/imagenes/informe.png" border="0" style="width:19px;height:19px; float: left; padding-left: 10px;" ></img>Paquete</a></li>	
+			<li><a class="s" href="#"><img src="<?php echo base_url(); ?>style/imagenes/informe.png" border="0" style="width:19px;height:19px; float: left; padding-left: 10px;">Incripcion</a></li>
 		</ul>
 	</li>
 	<li>
-		<a href="#" class="item op1Over2" rel="op1"><table style="width:167px;"><tr><td><img src="<?php echo base_url(); ?>style/imagenes/informe.png" border="0" style="width:20px;height:20px"></img></td><td><b> Generar Informe</b></td><tr></table></a>
+		<a href="#" class="item op1Over2" rel="op1"><img src="<?php echo base_url(); ?>style/imagenes/informe.png" border="0" style="width:20px;height:20px; float: left; padding-left: 10px;"></img><b> Generar Informe</b></a>
 	</li>
 	<li>
-		<a href="#" onclick="load_form()" class="item op1Over2" rel="op1"><table style="width:167px;"><tr><td><img src="<?php echo base_url(); ?>style/imagenes/logout.png" border="0" style="width:20px;height:20px"></img></td><td><b> Salir</b></td><tr></table></a>
+		<a href="#" onclick="load_form()" class="item op1Over2" rel="op1"><img src="<?php echo base_url(); ?>style/imagenes/logout.png" border="0" style="width:20px;height:20px; float: left; padding-left: 10px;;"></img><b> Salir</b></a>
 	</li>
 
 </ul>
