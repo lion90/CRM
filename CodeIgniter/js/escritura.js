2895d38b6256e47e4d0b91465b930a13d899247e
Marco Rivas <riv_mark1991rok@hotmail.com>
2013-03-28 00:32:37 -0600

    sdfd
    dfsd

--- CodeIgniter/js/escritura.js
+++ CodeIgniter/js/escritura.js
@@ -1,15 +1,22 @@
 /*css("background","url('localhost:81/probando/CRM/CodeIgniter/style/imagenes/12.png')")
 "background","url('localhost:81/probando/CRM/CodeIgniter/style/imagenes/12.png')"*/
-$(document).ready(function () {
-  $("#ing_encuesta").click(function(){
-$("#cuerpo").load("digitador/ing_encuesta");
+$(document).ready(function () 
+{
+  $("#ing_encuesta").click(function()
+  	{
+  		$("#cuerpo").load("digitador/ing_encuesta");
  	 });
-  $("#ing_openhouse").click(function(){
+
+  $("#ing_openhouse").click(function()
+  {
 	window.open("digitador/ing_openhouse")
- 	 });	
+	});	
+	
 	$("#enviar").mouseover(function(){$(this).css("opacity","1");});
-$("#enviar").mouseout(function(){$(this).css("opacity","0.7");});
-	});
+
+	$("#enviar").mouseout(function(){$(this).css("opacity","0.7");});
+
+});
 
 
 	
