ff9f88c5e1598de81d6a2db144ece66219f7b619
Marco Rivas <riv_mark1991rok@hotmail.com>
2013-04-03 01:02:58 -0600

    ss
    dfd

--- CodeIgniter/application/models/crm_model.php
+++ CodeIgniter/application/models/crm_model.php
@@ -9,7 +9,7 @@ class CRM_model extends CI_Model
 
    public function comprobar_usuario($nick, $pass)
    {  $password=md5($pass);
-      $query = $this->db->get_where('USERS', array('USER_NAME' => $nick,'PASSWORD' => $password));
+      $query = $this->db->get_where('users', array('USER_NAME' => $nick,'PASSWORD' => $password));
       return $query->row_array();
    }
 }
